e2e7fa5d5d9dacb9f0b8c6dc2cb0958caef160fc
ShoMasegi <m2.b0c27@gmail.com>
2018-03-24 01:56:23 +0900

    change EditLesson Layout

--- app/src/main/java/masegi/sho/classtable/presentation/views/editclass/EditLessonFragment.kt
+++ app/src/main/java/masegi/sho/classtable/presentation/views/editclass/EditLessonFragment.kt
@@ -3,17 +3,23 @@ package masegi.sho.classtable.presentation.views.editclass
 
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.os.bundleOf
 import dagger.android.support.DaggerFragment
 
 import masegi.sho.classtable.R
+import masegi.sho.classtable.data.Prefs
 import masegi.sho.classtable.databinding.FragmentEditLessonBinding
 import masegi.sho.classtable.kotlin.data.model.DayOfWeek
 import masegi.sho.classtable.kotlin.data.model.Lesson
+import masegi.sho.classtable.kotlin.data.model.ThemeColor
 import masegi.sho.classtable.presentation.NavigationController
 import masegi.sho.classtable.presentation.Result
 import masegi.sho.classtable.utli.ext.observe
@@ -54,13 +60,51 @@ class EditLessonFragment : DaggerFragment() {
 
             when(result) {
 
-                is Result.Success -> { binding.lesson = result.data }
-                is Result.Failure -> {
+                is Result.Success -> {
+
+                    binding.lesson = result.data
+                }
+                is Result.Failure, is Result.InProgress -> {
 
                     binding.lesson = Lesson(name = "", week = day, start = start)
                 }
             }
         }
+        setupViews()
+    }
+
+
+    // MARK: - Private
+
+    private fun setupViews() {
+
+        setupSpinner()
+        setupColorView()
+        binding.saveButton.setOnClickListener {
+
+            binding.lesson?.let { editLessonViewModel.save(it) }
+        }
+        binding.cancelButton.setOnClickListener { activity?.onBackPressed() }
+    }
+
+    private fun setupSpinner() {
+
+        val adapter: ArrayAdapter<Int> = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item)
+        for (i in 1 until Prefs.dayLessonCount) { adapter.add(i) }
+        binding.sectionSpinner.adapter = adapter
+    }
+
+    private fun setupColorView() {
+
+        val theme = binding.lesson?.theme ?: ThemeColor.DEFAULT
+        val drawable = GradientDrawable().apply {
+
+            shape = GradientDrawable.RECTANGLE
+            val scale = (resources.displayMetrics.density * 1.0).toInt()
+            setStroke(scale, ContextCompat.getColor(context!!, R.color.border))
+            setColor(ContextCompat.getColor(context!!, theme.primaryColorResId))
+        }
+        binding.colorView.background = drawable
     }
 
 
