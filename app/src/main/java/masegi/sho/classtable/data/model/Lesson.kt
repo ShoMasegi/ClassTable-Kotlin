b9c6b4c254f72c7efc3511042bc817ac83c97e62
ShoMasegi <m2.b0c27@gmail.com>
2018-04-16 18:37:15 +0900

    fixed lesson model

--- app/src/main/java/masegi/sho/classtable/data/model/Lesson.kt
+++ app/src/main/java/masegi/sho/classtable/data/model/Lesson.kt
@@ -23,7 +23,7 @@ data class Lesson(
         var section: Int = 1,
         var week: DayOfWeek,
         @Embedded var room: Room = Room(),
-        var teacher: String? = null,
+        var teacher: String = "",
         @Embedded var attendance: Attendance = Attendance(),
         var theme: ThemeColor = ThemeColor.DEFAULT
 ) : BaseObservable()
